0fef9330f428cf872932454b5845e8edb98d15b4
FGuerradecamargo <f.guerradecamargo@gmail.com>
2023-01-27 00:04:04 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function runGmame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+ function canculateCorrectAnswer() {
+
+ }
+
+ function incrementScore() {
+
+ }
+
+ function incrementWrongAnswer() {
+
+ }
+
+ function displayAdditionalQuestion() {
+
+ }
+
+ function displaySubtratQuestion() {
+
+ }
+
+ function displaymultiplyQuestion() {
+
+ }
+ 
