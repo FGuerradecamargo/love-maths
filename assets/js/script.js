40b7bed6fbaa1d62f7773d3f8a24009abb249c9e
FGuerradecamargo <f.guerradecamargo@gmail.com>
2023-01-27 00:25:52 +0000

    Add event lidtener for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+// Wait for the DOM to finish lading before runnin the game
+// Get the button elements and event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
 function runGmame() {
 
 }
@@ -29,4 +47,3 @@ function checkAnswer() {
  function displaymultiplyQuestion() {
 
  }
- 
