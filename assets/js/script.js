b098520c958b4deebce58270090e2c1acb110538
FGuerradecamargo <f.guerradecamargo@gmail.com>
2023-01-28 23:20:16 +0000

    Display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,21 +10,30 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGmame(gameType);
             }
         })
     }
+
+    runGmame("addition")
 })
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGmame() {
+function runGmame(gameType) {
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.randoma() * 25 +1);
     let num2 = Math.floor(Math.randoma() * 25 +1);
+
+    if (gameType === "addition") {
+        displayAdditionalQuestion(num1, num2);
+    } else {
+        alert(`Unknow game type: ${gameType}`);
+        throw `Unknow game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -43,8 +52,10 @@ function checkAnswer() {
 
  }
 
- function displayAdditionalQuestion() {
-
+ function displayAdditionalQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
  }
 
  function displaySubtratQuestion() {
