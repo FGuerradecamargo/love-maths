61a901d769c44a0fc4f44bf72397c193a1f4bd70
FGuerradecamargo <f.guerradecamargo@gmail.com>
2023-01-30 23:51:30 +0000

    Add divison function

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,8 @@ function runGmame(gameType) {
         displaymultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtratQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknow game type: ${gameType}`);
         throw `Unknow game type: ${gameType}. Aborting!`;
@@ -85,6 +87,8 @@ function checkAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 - operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`
@@ -121,8 +125,14 @@ function checkAnswer() {
 
 
 
- function displaymultiplyQuestion(operand1, operand2) {
+ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+ }
+
+ function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
  }
